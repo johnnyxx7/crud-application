4f74111c3c16e336a9c5ee360991ecf327b28c2c
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-26 14:05:51 -0300

    att commit

--- crud/src/main/java/com/john/crud/controllers/ProductController.java
+++ crud/src/main/java/com/john/crud/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.john.crud.controllers;
 
+import com.john.crud.model.Product;
+import com.john.crud.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    ProductRepository repository;
+
     @GetMapping
-    public String hello() {
-        return "hello!";
+    public ResponseEntity getAll() {
+        List<Product> productList = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
 }
